6d7c50a5398f80c9e2c3e2fc8a7e13e7f95a31dd
Alessandro-Moroni <alemoro98@gmail.com>
2024-03-20 14:10:12 +0100

    [did]repo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return{
+      title: 'Hello World!!!'
+    }
+  },
+
+
+}
+</script>
+
+<template>
+  <div class="square">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    color: orange;
+  }
+</style>
